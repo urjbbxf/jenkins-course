3cf8619d2bbd58868c202db5051eff739e702af7
urjbbxf <116504216+urjbbxf@users.noreply.github.com>
2023-05-10 15:13:22 +0200

    Update nodejsdocker.groovy
    user changed

--- job-dsl/nodejsdocker.groovy
+++ job-dsl/nodejsdocker.groovy
@@ -1,8 +1,8 @@
 job('NodeJS Docker example') {
     scm {
-        git('https://github.com/wardviaene/docker-demo.git') {  node -> // is hudson.plugins.git.GitSCM
-            node / gitConfigName('DSL User')
-            node / gitConfigEmail('jenkins-dsl@newtech.academy')
+        git('https://github.com/urjbbxf/docker-demo.git') {  node -> // is hudson.plugins.git.GitSCM
+            node / gitConfigName('urjbbxf')
+            node / gitConfigEmail('urjbbxf103@gmail.com')
         }
     }
     triggers {
@@ -14,7 +14,7 @@ job('NodeJS Docker example') {
     }
     steps {
         dockerBuildAndPublish {
-            repositoryName('wardviaene/docker-nodejs-demo')
+            repositoryName('urjbbxf/docker-nodejs-demo')
             tag('${GIT_REVISION,length=9}')
             registryCredentials('dockerhub')
             forcePull(false)
