6d63841dd9cdc4a5bdc07e436c9d7dfb70812506
urjbbxf <116504216+urjbbxf@users.noreply.github.com>
2023-05-10 15:12:18 +0200

    Update nodejs.groovy
    username and mail changed

--- job-dsl/nodejs.groovy
+++ job-dsl/nodejs.groovy
@@ -1,8 +1,8 @@
 job('NodeJS example') {
     scm {
-        git('https://github.com/wardviaene/docker-demo.git') {  node -> // is hudson.plugins.git.GitSCM
-            node / gitConfigName('DSL User')
-            node / gitConfigEmail('jenkins-dsl@newtech.academy')
+        git('https://github.com/urjbbxf/docker-demo.git') {  node -> // is hudson.plugins.git.GitSCM
+            node / gitConfigName('urjbbxf')
+            node / gitConfigEmail('urjbbxf103@gmail.com')
         }
     }
     triggers {
